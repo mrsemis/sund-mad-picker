d8a9d7fdcc8e954a1097490be18dda3505962357
mrsemis <121666205+mrsemis@users.noreply.github.com>
2025-07-18 17:40:48 +0200

    Create extrafeatures.js

---
+++ extrafeatures.js
@@ -0,0 +1,7 @@
+// Smooth scrolling på hele siden (for anchor links mm)
+document.documentElement.style.scrollBehavior = 'smooth';
+
+// Custom cursor - basic (kan ændres eller fjernes)
+document.body.style.cursor = 'default';
+
+// Kan smide flere random features her senere, bro!
